e0e42a99ce74983535a6ac58841f2cf52a581102
abdujemal <60457996+abdujemal@users.noreply.github.com>
2023-02-01 20:53:03 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:bussiness_management/view/controller/get_bindings.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
